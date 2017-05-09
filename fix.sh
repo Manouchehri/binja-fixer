0dd0769cee02887783fdcac5b57fbad15ec24c75
David Manouchehri <david.manouchehri@riseup.net>
2017-05-08 21:06:00 -0400

    Revert "Updated."
    This reverts commit 523b471aa0f0741cba0ae2434c7eb80204ae1a6d.

--- fix.sh
+++ fix.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# REAL_PY=`python2.7-config --prefix`
+REAL_PY=`python2.7-config --prefix`
 
 BN_PY_LB="/Applications/Binary Ninja.app/Contents/MacOS/plugins/libpythonplugin.dylib"
 
-# echo $REAL_PY
+echo $REAL_PY
 
-# cp -n "${REAL_PY}/Python" "${REAL_PY}/Python.bak"
+cp -n "${REAL_PY}/Python" "${REAL_PY}/Python.bak"
 
-# install_name_tool -change "/System/Library/Frameworks/Python.framework/Versions/2.7/Python" "${REAL_PY}/Python" "${BN_PY_LB}"
-
-install_name_tool -change "/System/Library/Frameworks/Python.framework/Versions/2.7/Python" "/usr/local/opt/python/Frameworks/Python.framework/Python" "${BN_PY_LB}"
-
-# This seems to work better now?
-install_name_tool -change "/usr/local/Cellar/python/2.7.13/Frameworks/Python.framework/Versions/2.7/Python" "/usr/local/opt/python/Frameworks/Python.framework/Python" "${BN_PY_LB}"
-
+install_name_tool -change "/System/Library/Frameworks/Python.framework/Versions/2.7/Python" "${REAL_PY}/Python" "${BN_PY_LB}"
 
 otool -L "${BN_PY_LB}"
 
